4129dce6ad809670940ff669031afa7216d2f6c0
alexwal <awal@berkeley.edu>
2018-05-12 14:31:59 -0700

    added source code

---
+++ background.js
@@ -0,0 +1,32 @@
+// background.js
+
+// Called when the user clicks on the browser action.
+chrome.browserAction.onClicked.addListener(function(tab) {
+  // Send a message to the active tab
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    var activeTab = tabs[0];
+    chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
+  });
+});
+
+// Duplicate the current tab
+function duplicateCurrentTab() {
+  chrome.tabs.getSelected(null, function(tab) {
+      chrome.tabs.duplicate(tab.id);
+  });
+}
+
+// Called when receives message from content.js
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  if(request.message === "duplicate_current_tab") {
+    duplicateCurrentTab();
+  }
+});
+
+// Called when user presses keyboard shortcut defined in manifest.json (after customized in chrome://extensions/shortcuts)
+chrome.commands.onCommand.addListener(function(command) {
+  console.log('Command:', command);
+  if (command == "duplicate-current-tab-keyboard-shortcut") {
+  duplicateCurrentTab();
+  }
+});
