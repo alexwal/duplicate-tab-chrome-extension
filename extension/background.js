6033ef8ce3f35daea97d184aab919d04943cc204
alexwal <awal@berkeley.edu>
2018-05-13 10:28:55 -0700

    no longer runs content script

--- extension/background.js
+++ extension/background.js
@@ -1,11 +1,11 @@
+// Author: Alex Walczak (awal@awal.io)
 // background.js
 
-// Called when the user clicks on the browser action.
+// Called when the user clicks on the browser action
 chrome.browserAction.onClicked.addListener(function(tab) {
-  // Send a message to the active tab
+  // Duplicate the active tab
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    var activeTab = tabs[0];
-    chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
+    duplicateCurrentTab();
   });
 });
 
@@ -15,33 +15,3 @@ function duplicateCurrentTab() {
       chrome.tabs.duplicate(tab.id);
   });
 }
-
-// Called when receives message from content.js
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  if(request.message === "duplicate_current_tab") {
-    duplicateCurrentTab();
-  }
-});
-
-// On startup, we inject each tab with new script to avoid a refresh of all pages.
-var injectExtensionCode = function() {
-  chrome.tabs.query({}, function(tabs) {
-    var i = 0, n = tabs.length;
-    for ( ; i < n; i++ ) {
-      var tab = tabs[i];
-      chrome.tabs.executeScript(tab.id, {file: "content.js"});
-    }
-  });
-}
-
-// Check whether new version is installed; inject scripts (see injectExtensionCode).
-chrome.runtime.onInstalled.addListener(function(details) {
-    if (details.reason == "install") {
-        // alert("This is a first install!");
-        injectExtensionCode();
-    } else if (details.reason == "update") {
-        var thisVersion = chrome.runtime.getManifest().version;
-        // alert("Updated from " + details.previousVersion + " to " + thisVersion + "!");
-        injectExtensionCode();
-    }
-});
--- extension/content.js
+++
@@ -1,9 +0,0 @@
-// content.js
-
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  if (request.message === "clicked_browser_action") {
-    console.log("Clicked duplicate tab button");
-    chrome.runtime.sendMessage({"message": "duplicate_current_tab"});
-    console.log("Sent duplicate tab request finished");
-  }
-});
