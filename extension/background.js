b1e7e7ca527ebfb98d6d0e21bd0423a1139f7bdc
alexwal <awal@berkeley.edu>
2018-05-13 15:41:16 -0700

    simplified code

--- extension/background.js
+++ extension/background.js
@@ -1,10 +1,9 @@
 // Author: Alex Walczak (awal@awal.io)
-// background.js
+
+// Duplicate tabs.Tab `tab`
+function duplicateTab(tab) {
+  chrome.tabs.duplicate(tab.id);
+}
 
 // Called when the user clicks on the browser action
-chrome.browserAction.onClicked.addListener(function(tab) {
-  // Duplicate the active tab in the current window
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.duplicate(tabs[0].id);
-  });
-});
+chrome.browserAction.onClicked.addListener(duplicateTab);
